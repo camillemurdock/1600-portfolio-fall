9009a73c552e0e22497746a3d54b5d8b5c5c70a5
Camille Murdock <clmurdo25@gmail.com>
2020-11-10 13:19:29 -0700

    added the java to the characters file

---
+++ star-wars-api/star-wars-characters/main.js
@@ -0,0 +1,60 @@
+import { people } from '../data/people.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
+
+const mainContent = document.querySelector('#main')
+
+populateDOM(people)
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' ||
+        person.gender === 'none' ||
+        person.gender === 'hermaphrodite') {
+        return person
+    }
+})
+
+maleButton.addEventListener('click', (event) => {
+   populateDOM(maleCharacters)
+})
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
