d9958d5a899edcb666f8f60bc6d803766476062e
Camille Murdock <clmurdo25@gmail.com>
2020-12-17 12:18:22 -0700

    commit for poke

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,41 +1,40 @@
 // Reusable async function to fetch data from the provided url
 async function getAPIData(url) {
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      return data
-    } catch (error) {
-      console.error(error)
-    }
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
   }
-  
-  // now, use the async getAPIData function
-  function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
-      //?limit=25&offset=800
-      async (data) => {
-        for (const pokemon of data.results) {
-          await getAPIData(pokemon.url).then((pokeData) => {
-            populatePokeCard(pokeData)
-          })
-        }
-      },
-    )
-  }
-  
+}
+ 
+function loadPage() {
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=30`).then(
+    async (data) => {
+      for (const pokemon of data.results) {
+        await getAPIData(pokemon.url).then((pokeData) => {
+          populatePokeCard(pokeData)
+        })
+      }
+    },
+  )
+}
+
   const pokeGrid = document.querySelector('.pokemonGrid')
   const loadButton = document.querySelector('.load')
   const newPokemonButton = document.querySelector('.newPokemon')
-  
-  newPokemonButton.addEventListener('click', () => {
-      let pokeName = prompt('What is your new Pokemon name?')
-    let newPokemon = new Pokemon(
-      pokeName,
-      400,
-      200,
-      ['gorge', 'sleep', 'cough'],
-      ['eat', 'study','code'])
-      populatePokeCard(newPokemon)
+
+
+newPokemonButton.addEventListener('click', () => {
+    let pokeName = prompt('What is your new Pokemon name?')
+  let newPokemon = new Pokemon(
+    pokeName,
+    400,
+    200,
+    ['gorge', 'sleep', 'cough'],
+    ['eat', 'study','code'])
+    populatePokeCard(newPokemon)
       // let's find pokemon by height or weight and combine to make new instances
   })
   
@@ -45,6 +44,7 @@ async function getAPIData(url) {
     loadButton.disabled = true
   })
   
+
   function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
@@ -71,36 +71,39 @@ async function getAPIData(url) {
     return pokeFront
   }
   
-  function populateCardBack(pokemon) {
-    let pokeBack = document.createElement('div')
-    pokeBack.className = 'card__face card__face--back'
-    let backLabel = document.createElement('p')
-    backLabel.textContent = `${pokemon.moves.length} moves`
-   //backLabel.addEventListener('click', () => getMovesDetails(pokemon.moves))
-    pokeBack.appendChild(backLabel)
-    return pokeBack
-  }
+function populateCardBack(pokemon) {
+  let pokeBack = document.createElement('div')
+  pokeBack.className = 'card__face card__face--back'
+  let backLabel = document.createElement('p')
+  backLabel.textContent = `${pokemon.moves.length} moves`
+  let backWeight = document.createElement('p')
+  backWeight.textContent = `${pokemon.weight} weight`
+ //backLabel.addEventListener('click', () => getMovesDetails(pokemon.moves))
+  pokeBack.appendChild(backLabel)
+  return pokeBack
+}
+
+
+function getMovesDetails(pokemonMoves) {
+
+  const nonNullMoves = pokemonMoves.filter(async (move) => {
+    if(!move.move) return
+    const moveData = await getAPIData(move.move.url)
+    console.log(moveData.accuracy, moveData.power)
+    if ((moveData.accuracy && moveData.power) !== null) {
+      return moveData
+    }
+  })
+  Promise.all(nonNullMoves).then((values) => {
+    console.log(values)
+  })
   
-  function getMovesDetails(pokemonMoves) {
   
-    const nonNullMoves = pokemonMoves.filter(async (move) => {
-      if(!move.move) return
-      const moveData = await getAPIData(move.move.url)
-      console.log(moveData.accuracy, moveData.power)
-      if ((moveData.accuracy && moveData.power) !== null) {
-        return moveData
-      }
-    })
-    Promise.all(nonNullMoves).then((values) => {
-      console.log(values)
-    })
-    
-    
-  /*   const result = pokemonMoves.reduce(async (acc, move) => {
-      const moveData = await getAPIData(move.move.url)
-      console.log(moveData.accuracy, moveData.power)
-    }, {}) */
-  }
+/*   const result = pokemonMoves.reduce(async (acc, move) => {
+    const moveData = await getAPIData(move.move.url)
+    console.log(moveData.accuracy, moveData.power)
+  }, {}) */
+}
   
   function getImageFileName(pokemon) {
     if (pokemon.id < 10) {
@@ -121,3 +124,4 @@ async function getAPIData(url) {
     this.id = 900
     this.moves = moves
   }
+  
