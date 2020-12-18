5eb518d12fee7974c76007a572a4f39a13a39585
Camille Murdock <clmurdo25@gmail.com>
2020-12-18 13:33:13 -0700

    added some fixes to films page

--- star-wars-api/star-wars-films/main.js
+++ star-wars-api/star-wars-films/main.js
@@ -1,17 +1,38 @@
 import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('main')
+
 
 for (let i = 0; i < 7; i++) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = films[i].title
+
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+
+    figCaption.textContent = foundFilm.title
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
 
     main.appendChild(figure)
-}
-  
+    console.log(films[i].title)
+  }
+
+/* films.forEach(film => {
+    console.log(film.title)
+    let newTitle = main.appendChild(document.createElement("h1"))
+    newTitle.textContent = film.title
+}) */
+
+/* people.forEach(person => {
+    console.log(person.name)
+    let personImg = document.createElement('img')
+    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
+    main.appendChild(personImg)
+}) */
+
+//https://starwars-visualguide.com/assets/img/films/7.jpg
+
